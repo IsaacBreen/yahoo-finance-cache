7d9cc73f0d5c3ead71968d3c0f9ce6d2c598a67a
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-07-12 23:40:26 +0800

    first commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+A simple library for caching Yahoo finance data. Built on top of Pandas DataReader.
+
+```python
+from cached_yahoo_finance import CachedYahooDataReader
+
+
+# Store the cached data in a directory called 'cache'
+yahoo_data_reader = CachedYahooDataReader('cache')
+
+# Download stock data for Apple from Yahoo Finance
+df = yahoo_data_reader.DataReader('AAPL', start_date='2021-01-01', end_date='2021-01-31')
+print(df)
+
+# Get the same data from the cache (will be much faster than downloading again)
+df2 = yahoo_data_reader.DataReader('AAPL', start_date='2021-01-01', end_date='2021-01-31')
+assert df.equals(df2), 'Data from cache and from Yahoo Finance should be the same'
+```
---
+++ tests/test_cached_yahoo_finance.py
@@ -0,0 +1,18 @@
+from yahoo_finance_cache import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_tutorial():
+    # Store the cached data in a directory called 'cache'
+    yahoo_data_reader = CachedYahooDataReader('cache')
+
+    # Download stock data for Apple from Yahoo Finance
+    df = yahoo_data_reader.DataReader('AAPL', start_date='2021-01-01', end_date='2021-01-31')
+    print(df)
+
+    # Get the same data from the cache (will be much faster than downloading again)
+    df2 = yahoo_data_reader.DataReader('AAPL', start_date='2021-01-01', end_date='2021-01-31')
+    assert df.equals(df2), 'Data from cache and from Yahoo Finance should be the same.'
