93ae4452d0c854399b8b6eb79c979fc878e1ce56
IsaacBreen <57783927+IsaacBreen@users.noreply.github.com>
2022-07-29 22:19:50 +0800

    fixed issue causing tickers with a period in them to throw an error

--- tests/test_cached_yahoo_finance.py
+++ tests/test_cached_yahoo_finance.py
@@ -18,3 +18,19 @@ def test_tutorial():
     # Get the same data from the cache (will be much faster than downloading again)
     df2 = yahoo_data_reader.DataReader('AAPL', start_date='2021-01-01', end_date='2021-01-31')
     assert df.equals(df2), 'Data from cache and from Yahoo Finance should be the same.'
+
+
+def test_dot_in_ticker():
+    from yahoo_finance_cache import CachedYahooDataReader
+    
+    TICKER = 'BRK.B'
+    
+    # Cache the data in a directory called 'cache'
+    yahoo_data_reader = CachedYahooDataReader('cache')
+
+    # Download stock data for Apple from Yahoo Finance
+    df = yahoo_data_reader.DataReader(TICKER, start_date='2021-01-01', end_date='2021-01-31')
+
+    # Get the same data from the cache (will be much faster than downloading again)
+    df2 = yahoo_data_reader.DataReader(TICKER, start_date='2021-01-01', end_date='2021-01-31')
+    assert df.equals(df2), 'Data from cache and from Yahoo Finance should be the same.'
--- yahoo_finance_cache/yahoo_data_reader_cached.py
+++ yahoo_finance_cache/yahoo_data_reader_cached.py
@@ -13,8 +13,8 @@ class CachedYahooDataReader:
     """
     Get Yahoo finance data speedily with a local cache.
     """
-    
-    def __init__(self, cache_dir='cache'):
+
+    def __init__(self, cache_dir="cache"):
         self.cache_dir = pathlib.Path(cache_dir)
 
     @staticmethod
@@ -22,11 +22,14 @@ class CachedYahooDataReader:
         """
         Get Yahoo Finance data for a given ticker.
         """
-        df = web.DataReader(ticker, 'yahoo', start_date, end_date)
+        df = web.DataReader(ticker, "yahoo", start_date, end_date)
         # Cast to float
         df = df.astype(float)
         # The stock market is not open every day, so add rows with NaN for in-between days
-        dates = [start_date + datetime.timedelta(n) for n in range((end_date - start_date).days + 1)]
+        dates = [
+            start_date + datetime.timedelta(n)
+            for n in range((end_date - start_date).days + 1)
+        ]
         df_filled = pd.DataFrame(index=dates, columns=df.columns)
         df_filled.update(df)
         return df_filled
@@ -43,21 +46,29 @@ class CachedYahooDataReader:
             end_date = dateparser.parse(end_date)
         try:
             # Open cached data with datetime objects as the indices
-            df = pd.read_csv(self.cache_dir / f'{ticker}.csv', index_col=0, parse_dates=True)
+            df = pd.read_csv(
+                self.cache_dir / f"{ticker.replace('.', '-')}.csv", index_col=0, parse_dates=True
+            )
             df.astype(float)
         except FileNotFoundError:
             # Get data from Yahoo Finance
-            logging.info('Cache miss for {}. Getting data from Yahoo Finance.'.format(ticker))
-            df = self.get_yahoo_data(ticker, start_date, end_date)
+            logging.info(
+                "Cache miss for {}. Getting data from Yahoo Finance.".format(ticker)
+            )
+            df = self.get_yahoo_data(ticker.replace('.', '-'), start_date, end_date)
             # Create the cache directory if it doesn't exist
-            pathlib.Path('cache').mkdir(parents=True, exist_ok=True)
+            pathlib.Path("cache").mkdir(parents=True, exist_ok=True)
             # Save data to cache
-            df.to_csv(self.cache_dir / f'{ticker}.csv')
+            df.to_csv(self.cache_dir / f"{ticker.replace('.', '-')}.csv")
         # Get the data for the given date range
         df = df[(df.index >= start_date) & (df.index <= end_date)]
         # Check there is a row for each day in the date range and download any missing data
         if len(df) != (end_date - start_date).days + 1:
-            logging.info('Cache miss on some days for {}. Getting data from Yahoo Finance.'.format(ticker))
+            logging.info(
+                "Cache miss on some days for {}. Getting data from Yahoo Finance.".format(
+                    ticker
+                )
+            )
             df_new = self.get_yahoo_data(ticker, start_date, end_date)
             # Remove any overlapping data
             df_new = df_new[~df_new.index.isin(df.index)]
@@ -66,17 +77,17 @@ class CachedYahooDataReader:
             # Sort date
             df = df.sort_index()
             # Cache it
-            df.to_csv(self.cache_dir / f'{ticker}.csv')
+            df.to_csv(self.cache_dir / f"{ticker.replace('.', '-')}.csv")
         # Remove NaN rows
-        df = df.dropna(how='all')
+        df = df.dropna(how="all")
         return df
-    
-    def DataReader(self, ticker, source='yahoo', start_date=None, end_date=None):
+
+    def DataReader(self, ticker, source="yahoo", start_date=None, end_date=None):
         """
         Get Yahoo Finance data for a given ticker.
         """
-        assert start_date is not None, 'start_date must be specified'
+        assert start_date is not None, "start_date must be specified"
         if end_date is None:
             end_date = datetime.datetime.now()
-        assert source == 'yahoo', 'Only Yahoo Finance data is supported.'
+        assert source == "yahoo", "Only Yahoo Finance data is supported."
         return self.get_cached_yahoo_data(ticker, start_date, end_date)
